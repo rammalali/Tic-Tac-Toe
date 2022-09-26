3788412309083488772519770ea18269246801ea
Ali Rammal <Ali.rammal.4@hotmail.com>
2022-09-26 19:12:41 +0300

    implementing createBoard - printBoard - spaceIsFree - insertLetter Methods !

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,22 +1,54 @@
-def create_board(board):
+def create_board(board_input):
     for i in range(1, 10):
-        board[i] = ' ';
-    return board;
+        board_input[i] = ' '
+    return board_input
+
 
 board = create_board({})
 
 
-def printBoard(board):
+def printBoard(board_input):
     for i in range(3):
         line = ''
         for j in range(1, 4):
-            line = line + '|' + board[j+i*3]
+            line = line + '|' + board_input[j + i * 3]
         line += '|'
         print(" - - - ")
         print(line)
 
-printBoard(board)
 
-# def spaceIsFree(position):
-#
 
+def spaceIsFree(position):
+    if board[position] == ' ':
+        return True
+    else:
+        return False
+
+def insertLetter(letter, position):
+    if spaceIsFree(position):
+        board[position] = letter
+        printBoard(board)
+        if checkDraw():
+            print("Draw!")
+            exit()
+        if checkWin():
+            if letter == 'X':
+                print("Bot Win !")
+                exit()
+            else:
+                print("Player Wins!")
+                exit()
+        return
+
+
+    else:
+        print("can't Insert there!")
+        position = int(input("Enter New position: "))
+        insertLetter(letter, position)
+
+def checkDraw():
+    pass
+
+
+def checkWin():
+    pass
