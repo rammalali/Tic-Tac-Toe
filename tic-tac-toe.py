533643834443f5e1e3ab645f3fe9f8a8046348d7
ali_rammal <ali.rammal.4@hotmail.com>
2022-10-03 22:01:52 +0300

    finish minimax, we can start with tkinter gui then we try expectiMax

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -75,12 +75,85 @@ def checkWin():
         return False
 
 
-def playerMore():
+def checkWhoWon(letter):
+    if board[1] == board[2] and board[1] == board[3] and board[1] == letter:
+        return True
+    elif board[4] == board[5] and board[4] == board[6] and board[4] == letter:
+        return True
+    elif board[7] == board[8] and board[7] == board[9] and board[7] == letter:
+        return True
+    elif board[1] == board[4] and board[1] == board[7] and board[1] == letter:
+        return True
+    elif board[2] == board[5] and board[2] == board[8] and board[2] == letter:
+        return True
+    elif board[3] == board[6] and board[3] == board[9] and board[3] == letter:
+        return True
+    elif board[1] == board[5] and board[1] == board[9] and board[1] == letter:
+        return True
+    elif board[3] == board[5] and board[3] == board[7] and board[3] == letter:
+        return True
+
+    else:
+        return False
+
+
+def playerMove():
     position = int(input("Position O:"))
     insertLetter('O', position)
     return
 
+
 def comMove():
-    position = int(input("Position X:"))
-    insertLetter('X', position)
-    return
+    bestScore = -1
+    bestMove = 0  # will be changed
+
+    for key in board.keys():
+        if board[key] == ' ':
+            board[key] = 'X'
+            score = minimax(board, False)
+            board[key] = ' '
+
+            if score > bestScore:
+                bestScore = score
+                bestMove = key
+    insertLetter('X', bestMove)
+
+
+def minimax(board, isMaximizing):
+    if checkWhoWon('X'):
+        return 1
+    elif checkWhoWon('O'):
+        return -1
+    elif checkDraw():
+        return 0
+
+    if isMaximizing:
+        bestScore = -1
+
+        for key in board.keys():
+            if board[key] == ' ':
+                board[key] = 'X'
+                score = minimax(board, False)
+                board[key] = ' '
+
+                if score > bestScore:
+                    bestScore = score
+        return bestScore
+    else:
+
+        bestScore = 1
+
+        for key in board.keys():
+            if board[key] == ' ':
+                board[key] = 'O'
+                score = minimax(board, True)
+                board[key] = ' '
+
+                if score < bestScore:
+                    bestScore = score
+        return bestScore
+
+
+while not checkWin():
+    comMove()
+    playerMove()
